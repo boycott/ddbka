33c66ae58dd3dbc6016b508dde1035a876cf030c
Geoff Kaile <383343+boycott@users.noreply.github.com>
2025-12-01 18:53:04 +0000

    Added vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ddbka.co.uk",
+  output: "static",
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
