09c81a42e30c4da550549c0e7b319dbc0edd6299
Geoff Kaile <383343+boycott@users.noreply.github.com>
2026-04-17 13:49:35 +0100

    Added new YLAH video and few fixes

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -2,13 +2,13 @@ import { getCollection } from "astro:content";
 import { getPastCollection, getUpcomingCollection } from "./global";
 
 export const getUpcoming = async (limit?: number) => {
-  const allMeetings = await getCollection("meetings");
+  const allMeetings = await getCollection("events");
 
   return getUpcomingCollection(allMeetings, limit);
 };
 
 export const getPast = async (limit?: number) => {
-  const allMeetings = await getCollection("meetings");
+  const allMeetings = await getCollection("events");
 
   return getPastCollection(allMeetings, limit);
 };
